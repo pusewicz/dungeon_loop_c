1dad4c46678a654719b736250c896cccc407a678
Piotr Usewicz <piotr@layer22.com>
2024-09-02 19:47:56 +0200

    Cleanup main file

--- src/dungeon_loop.c
+++ src/dungeon_loop.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
-#include <execinfo.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <SDL3/SDL.h>
 #include "cute.h"
 
 int main(int argc, char* argv[]) {
-  printf("Hello from %s %d\n", CF_VERSION_STRING_COMPILED, SDL_GPU_TEXTUREFORMAT_D24_UNORM_S8_UINT);
   CF_Result result = cf_make_app("Dungeon Loop", cf_default_display(), 0, 0, 320, 240, CF_APP_OPTIONS_WINDOW_POS_CENTERED_BIT, argv[0]);
   if(cf_is_error(result)) {
-    printf("Error creating app");
+    printf("Error creating app: %s", result.details);
     return EXIT_FAILURE;
   }
-  
+
   CF_Sprite girl = cf_make_demo_sprite();
   cf_sprite_play(&girl, "spin");
   cf_sprite_set_scale(&girl, cf_v2(4,4));
